ff9bba7d68ddac78df92d23100e0e0762119ca5f
SportKight <1014841561@qq.com>
2023-04-18 09:14:14 +0800

    更改为适用于 Series 的四元数转为欧拉角

--- zy_Inshoe_Data_IMU_plot/transform_euler_angles.py
+++ zy_Inshoe_Data_IMU_plot/transform_euler_angles.py
@@ -1,16 +1,18 @@
 # %%
 import numpy as np
+import pandas as pd
 from scipy.spatial.transform import Rotation
 
 def transform_euler_angles(quaternion):
     """
     使用四元数转换欧拉角，并将其转换为特定形式的欧拉角。
+    适用于鞋垫传感器
     
     Args:
-        quaternion (list): 长度为4的列表，表示四元数。
+        quaternion (list): 长度为4的Series，表示四元数。
     
     Returns:
-        list: 长度为3的列表，表示经过转换的欧拉角。
+        pd.Series: 长度为3的Series，表示经过转换的欧拉角。
     """
     # 使用四元数转换欧拉角
     rotation = Rotation.from_quat(quaternion)
@@ -21,7 +23,7 @@ def transform_euler_angles(quaternion):
     # 第一个角度不需要变化，第二个角度前面加负数，第三个是和180互补加负数
     transformed_angles = np.round([euler_angles[0], -euler_angles[1], -(180 - euler_angles[2])], 6)
     
-    return transformed_angles
+    return pd.Series(transformed_angles)
 
 # %%
 
