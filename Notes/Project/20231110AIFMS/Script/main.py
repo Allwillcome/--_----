3222233dd72404c4af630e148dda905c821461d8
SportKight <1014841561@qq.com>
2023-11-23 14:19:59 +0800

    完成初步方案生成的GUI界面

--- Notes/Project/20231110AIFMS/Script/main.py
+++ Notes/Project/20231110AIFMS/Script/main.py
@@ -1,8 +1,11 @@
+import os
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QFileDialog, QLabel
 import pandas as pd
+import subprocess #Mac 电脑专用，调用 LibreOffice
 from reportlab.lib import pagesizes
 from reportlab.pdfgen import canvas
+from PyPDF2 import PdfReader, PdfWriter
 
 class ExcelToPDFConverter(QWidget):
     def __init__(self):
@@ -11,21 +14,21 @@ class ExcelToPDFConverter(QWidget):
     
     def initUI(self):
         # 设置窗口的标题和大小
-        self.setWindowTitle("Excel 到 PDF 转换器")
+        self.setWindowTitle("智能 FMS功能性筛查 训练方案")
         self.setGeometry(300, 300, 350, 200)
 
         # 布局和组件
         layout = QVBoxLayout()
 
-        self.label = QLabel('请选择 Excel 文件', self)
+        self.label = QLabel('📢请选择原始评分文件', self)
         layout.addWidget(self.label)
 
-        select_button = QPushButton("选择文件", self)
+        select_button = QPushButton("📜请选择评分文件", self)
         select_button.clicked.connect(self.openFileNameDialog)
         layout.addWidget(select_button)
 
-        print_button = QPushButton("转换为 PDF", self)
-        print_button.clicked.connect(self.print_to_pdf)
+        print_button = QPushButton("🏋️生成训练方案", self)
+        print_button.clicked.connect(self.ExcelToPdf)
         layout.addWidget(print_button)
 
         self.setLayout(layout)
@@ -37,27 +40,33 @@ class ExcelToPDFConverter(QWidget):
         if fileName:
             self.label.setText(fileName)
 
-    def print_to_pdf(self):
-        file_path = self.label.text()
+    # 调用 Libreoffice 将 excel 导出为 pdf
+    def ExcelToPdf(self):
+        excel_path = self.label.text()
+        excel_dir,_ = os.path.split(excel_path)
+        output_pdf_path = excel_dir
+        pdf_path = excel_path.replace(".xlsx", ".pdf")
         try:
-            df = pd.read_excel(file_path, sheet_name="输出报告")
+            subprocess.run(["/Applications/LibreOffice.app/Contents/MacOS/soffice", 
+                            "--headless", "--convert-to", "pdf", excel_path, 
+                            "--outdir", output_pdf_path], check=True)
+            print("打印成功")
 
-            pdf_path = file_path.replace(".xlsx", ".pdf")
-            c = canvas.Canvas(pdf_path, pagesizes.letter)
+            reader = PdfReader(pdf_path)
+            writer = PdfWriter()
 
-            width, height = pagesizes.letter
+            # 获取最后一页并添加到新的 PDF
+            last_page = reader.pages[-1]  
+            writer.add_page(last_page)  
 
-            text_obj = c.beginText(40, height - 40)
-            for index, row in df.iterrows():
-                line = ", ".join(map(str, row))
-                text_obj.textLine(line)
-
-            c.drawText(text_obj)
-            c.save()
-
-            self.label.setText("PDF 已保存至："+ pdf_path)
-        except Exception as e:
-            self.label.setText("错误：" + str(e))
+            # 保存新的 PDF
+            output_pdf_path =  pdf_path
+            with open(output_pdf_path, "wb") as output_pdf:
+                writer.write(output_pdf)
+            self.label.setText("✅解决方案 已保存至：\n"+ pdf_path)
+        except subprocess.CalledProcessError as e:
+            print("生成方案失败:\n ", e)
+    
     
 if __name__ == "__main__":
     app = QApplication(sys.argv)
